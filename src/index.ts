5ae612764f2d58035fe7d99dec060f3c6ee69c9c
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-09-16 16:25:46 -0500

    Observable and subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { Observable } from 'rxjs'
+import { Observable, Observer } from 'rxjs'
+
+const observer: Observer<string> = {
+  next: (value) => console.log('next', value),
+  error: (error) => console.warn('error', error),
+  complete: () => console.info('complete')
+}
 
 const obs$ = new Observable<string>(subscriber => {
   // Emit values
@@ -8,6 +14,10 @@ const obs$ = new Observable<string>(subscriber => {
   subscriber.next('Hello')
   subscriber.next('World')
 
+  // Force an error
+  /* const error = undefined
+  error.name = 'Test' */
+
   subscriber.complete() // complete the observable
 
   // This will not be emitted because the observable is completed
@@ -15,4 +25,10 @@ const obs$ = new Observable<string>(subscriber => {
   subscriber.next('World')
 })
 
-obs$.subscribe(console.log)
+/* obs$.subscribe(
+  value => console.log('next:', value),
+  error => console.warn('error:', error),
+  () => console.info('completed')
+) */
+
+obs$.subscribe(observer)
