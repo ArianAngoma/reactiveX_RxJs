094c7b0768a91f528bf4e3de2eb5544afc03ddbb
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-09-16 17:14:03 -0500

    Subscription and unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,31 @@ const observer: Observer<string> = {
   error: (error) => console.warn('error', error),
   complete: () => console.info('complete')
 }
+
+const interval$ = new Observable<number>(subscriber => {
+  let count = 0
+
+  const interval = setInterval(() => {
+    count++
+    subscriber.next(count)
+
+    console.log(count)
+  }, 1000)
+
+  return () => {
+    clearInterval(interval)
+    console.log('Interval destroyed')
+  }
+})
+
+const subscription1 = interval$.subscribe()
+const subscription2 = interval$.subscribe()
+const subscription3 = interval$.subscribe()
+
+setTimeout(() => {
+  subscription1.unsubscribe()
+  subscription2.unsubscribe()
+  subscription3.unsubscribe()
+
+  console.log('Completed timeout')
+}, 3000)
