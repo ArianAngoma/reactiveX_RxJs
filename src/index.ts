9c46a5ecf0e9ce7edc34ddf637a6cc246ad761c0
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-09-16 16:34:07 -0500

    Folder structure

--- src/index.ts
+++ src/index.ts
@@ -5,30 +5,3 @@ const observer: Observer<string> = {
   error: (error) => console.warn('error', error),
   complete: () => console.info('complete')
 }
-
-const obs$ = new Observable<string>(subscriber => {
-  // Emit values
-  subscriber.next('Hello')
-  subscriber.next('World')
-
-  subscriber.next('Hello')
-  subscriber.next('World')
-
-  // Force an error
-  /* const error = undefined
-  error.name = 'Test' */
-
-  subscriber.complete() // complete the observable
-
-  // This will not be emitted because the observable is completed
-  subscriber.next('Hello')
-  subscriber.next('World')
-})
-
-/* obs$.subscribe(
-  value => console.log('next:', value),
-  error => console.warn('error:', error),
-  () => console.info('completed')
-) */
-
-obs$.subscribe(observer)
